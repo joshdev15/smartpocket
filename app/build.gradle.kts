0653de378be196971d0d27cd0ac7bc15fd8d2704
Joshua Mora <joshuaalexandermora@gmail.com>
2025-11-22 19:03:12 -0400

    Version: 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.joshdev.smartpocket"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
